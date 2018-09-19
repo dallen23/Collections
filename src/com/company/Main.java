85107520014f29add2599170ccc42a3bc3ed3aa8
dallen23 <dallen23@my.wctc.edu>
2018-09-19 17:07:20 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,51 @@
+package com.company;
+
+import java.util.*;
+
+public class Main {
+
+    private final static FileInput indata = new FileInput("the_book.csv");
+    private final static HashMap<String, Integer> map = new HashMap<String, Integer>();
+
+    public static void main(String[] args) {
+        String line;
+        String[] words;
+        Object wordFound;
+        //   String[] fields;
+
+        while ((line = indata.fileReadLine()) != null) {
+            line=line.replace(",","").replace(".","")
+                    .replace(";","").replace(":","")
+                    .replace("'","").replace("\"","")
+                    .replace("-","").replace("!","")
+                    .replace("#","").replace("(","")
+                    .replace(")","").replace("?","")
+                    .replace("_"," ").replace("?","")
+                    .toLowerCase().trim();
+            words = line.split(" ");
+            for (String s:words) {
+                wordFound = map.get(s);
+                if (wordFound == null) {
+                    map.put(s, new Integer(1));
+                }
+                else {
+                    map.put(s, map.get(s) + 1);
+                }
+
+            }
+        }
+        Object[] a = map.entrySet().toArray();
+        Arrays.sort(a, new Comparator() {
+            public int compare(Object o1, Object o2) {
+                return ((Map.Entry<String, Integer>) o2).getValue()
+                        .compareTo(((Map.Entry<String, Integer>) o1).getValue());
+            }
+        });
+        for (Object e : a) {
+            System.out.println(((Map.Entry<String, Integer>) e).getKey() + " : "
+                    + ((Map.Entry<String, Integer>) e).getValue());
+        }
+    }
+
+
+}
